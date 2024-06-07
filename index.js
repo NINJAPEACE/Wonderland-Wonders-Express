4890053b227c32e63c19270ce003a121fc984958
NINJA PEACE <71932430+NINJAPEACE@users.noreply.github.com>
2024-06-07 12:20:05 +0000

    push test
    test

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
+const express = require("express"), app = express();
+const mongoose = require("mongoose");
 
-keepAlive();
-const monitor = new Monitor({
-    website: 'https://0137b1cf-c204-4849-bf00-14452970fc6f-00-3rex8pf0sl8of.sisko.replit.dev:8080/',
-    title: 'NAME',
-    interval: 2
+mongoose.connect("mongodb+srv://ninjabro:wtfman@cluster0.spf3bd5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {});
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error))
+
+const {Schema, model} = mongoose;
+
+const timeSchema = new Schema({
+  username: String,
+  time: Number
+})
+
+
+const Data = model("UserData", timeSchema);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/hareishere", function (req, res) {
+  res.render("index");
 });
 
-monitor.on('up', (res) => console.log(`${res.website} its on.`));
-monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
-monitor.on('error', (error) => console.log(error));
+app.get("/absolemganteng123", function (req, res) {
+  res.render("absolem");
+});
+
+app.get("/datasee", async(req, res) => {
+  const data  = await Data.find({});
+  res.render("datasee", {data});
+});
+
+app.get("*", function(req, res) {
+  res.render("404");
+})
+
+app.post("/submit", async(req, res) => {
+  console.log(req.body);
+  if(req.body.password == "eatme") {
+    await Data.create({username: req.body.username, time: Date.now()});
+    res.redirect("https://github.com/NINJAPEACE");
+  } else if(req.body.password == "drinkme") {
+    res.redirect("https://tryitands.ee");
+  }
+})
+
+ app.listen(3000, function () {
+    console.log("Server is running on port 3000 ");
+  });
+
+module.exports = app;
